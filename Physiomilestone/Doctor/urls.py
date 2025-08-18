93f4cc0825d796dd34e2f4c58aee103f4fd109a7
mmuzammil2003 <muzammilamir03@gmail.com>
2025-08-18 23:09:58 +0530

    do

--- Physiomilestone/Doctor/urls.py
+++ Physiomilestone/Doctor/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("Dashboard/",views.DashboardView.as_view(),name="ddashboard"),
+    path("AssignExercise/",views.AssignExerciseView.as_view(),name="assignex"),
+    path("PatientDetails/",views.PatientDetailView.as_view(),name="pdetails"),
+    path("Uploadexercise/",views.UploadExerciseView.as_view(),name="uploadexercise"),
+    path("patients/",views.PatientView.as_view(),name="patients"),
 ]
--- Physiomilestone/Doctor/views.py
+++ Physiomilestone/Doctor/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from USER.models import CustomUSer
 # Create your views here.
 class DashboardView(TemplateView):
     template_name="Doctor/Dashborad.html"
+
+class AssignExerciseView(TemplateView):
+    template_name="Doctor/Assign_Exercise.html"
+
+class PatientDetailView(TemplateView):
+    template_name="Doctor/Patient_Detail.html"
+    
+class UploadExerciseView(TemplateView):
+    template_name="Doctor/Upload_Exercise_Template.html"
+
+class PatientView(TemplateView):
+    template_name="Doctor/patients.html"
+    
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        children=CustomUSer.objects.filter(role="child")
+        context["patients"]=children
+        return context
