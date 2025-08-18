9a57c2fc15cf5b3efdb267ac7953a6652c863575
mmuzammil2003 <muzammilamir03@gmail.com>
2025-08-19 00:16:11 +0530

    daas

--- Physiomilestone/Doctor/forms.py
+++ Physiomilestone/Doctor/forms.py
@@ -1,5 +1,7 @@
-from django import forms
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
 from .models import Exercise
+from django import forms
 
 class Exerciseform(forms.ModelForm):
     class Meta:
@@ -8,3 +10,12 @@ class Exerciseform(forms.ModelForm):
                 "description",
                 "instruction",
                 "youtube_id"]
+        
+class UploadExerciseView(FormView):
+    template_name = "Doctor/Upload_Exercise.html"
+    form_class = Exerciseform
+    success_url = reverse_lazy("ddashboard")
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
--- Physiomilestone/Doctor/views.py
+++ Physiomilestone/Doctor/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from USER.models import CustomUSer
 from Doctor.models import Exercise
+from django.shortcuts import redirect
+from django.views import View
+from .models import Exercise
 # Create your views here.
 class DashboardView(TemplateView):
     template_name="Doctor/Dashborad.html"
@@ -22,8 +25,29 @@ class AssignExerciseView(TemplateView):
 class PatientDetailView(TemplateView):
     template_name="Doctor/Patient_Detail.html"
     
-class UploadExerciseView(TemplateView):
-    template_name="Doctor/Upload_Exercise_Template.html"
+
+
+class UploadExerciseView(View):
+    template_name = "Doctor/Upload_Exercise_Template.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        name = request.POST.get("name")
+        description = request.POST.get("description")
+        instruction = request.POST.get("instruction")
+        youtube_id = request.POST.get("youtube_id")
+
+        Exercise.objects.create(
+            name=name,
+            description=description,
+            instruction=instruction,
+            youtube_id=youtube_id
+        )
+
+        return redirect("ddashboard")
+
 
 class PatientView(TemplateView):
     template_name="Doctor/patients.html"
